e443f3c51fcd63403ea4000cb203373f08409431
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-04 12:54:44 -0700

    fixed imports of express, and re added nodemon, added custom logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const express = require('express')
+
+const server = require('./server.js')
+
+const port = 8008
+
+server.listen(port, () => console.log(`\nlistening on port ${port}\n`))
