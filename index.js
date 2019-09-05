41c7991bf067a7db6f91c85dd5aeddcd4ca7b6c3
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-05 12:43:26 -0700

    working on deploying my branch: add dotenv, added start: node index.js, added requires and process

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // code away!
+require('dotenv').config()
+
 const express = require('express')
 
 const server = require('./server.js')
 
-const port = 8008
+const port = process.env.PORT || 8008;
 
 server.listen(port, () => console.log(`\nlistening on port ${port}\n`))
